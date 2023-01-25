63de7dcb973d5f6426889ac2469934b4bc184e8b
smashplays <jesusterres1999@gmail.com>
2023-01-25 18:56:03 +0100

    API Routes Changed

--- AnimeTrackerBackend/routes/api.php
+++ AnimeTrackerBackend/routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Controllers\RolController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\ActorController;
 use App\Http\Controllers\CharacterController;
-use App\Http\Controllers\MicrophoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 /*
@@ -36,21 +34,6 @@ Route::prefix('/users')->group(function () {
     Route::middleware('validate.id')->get('/{id}/calendar', [UserController::class, 'calendar']);
 });
 
-Route::prefix('/rols')->group(function () {
-
-    Route::get('', [RolController::class, 'getAll']);
-
-    Route::middleware('validate.id')->get('/{id}', [RolController::class, 'getById']);
-
-    Route::middleware('validate.id')->get('/{id}/users', [RolController::class, 'users']);
-
-    Route::post('', [RolController::class, 'post']);
-
-    Route::middleware('validate.id')->delete('/{id}', [RolController::class, 'delete']);
-
-    Route::middleware('validate.id')->patch('/{id}', [RolController::class, 'update']);
-});
-
 Route::prefix('/calendars')->group(function () {
 
 
@@ -68,38 +51,21 @@ Route::prefix('/calendars')->group(function () {
     Route::middleware('validate.id')->get('/{id}/user', [CalendarController::class, 'user']);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-    // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    //     return $request->user();
-    // });
-
-    Route::prefix('/actors')->group(function () {
-        Route::get('', [ActorController::class, 'getAll']);
-        Route::middleware('validate.id')->get('/{id}', [ActorController::class, 'getById']);
-        Route::middleware('validate.id')->get('/{id}/characters', [ActorController::class, 'characters']);
-        Route::middleware('validate.id')->get('/{id}/microphone', [ActorController::class, 'microphone']);
-        Route::post('', [ActorController::class, 'create']);
-        Route::middleware('validate.id')->delete('/{id}', [ActorController::class, 'delete']);
-        Route::middleware('validate.id')->patch('/{id}', [ActorController::class, 'modify']);
-    });
-
-    Route::prefix('/characters')->group(function () {
-        Route::get('', [CharacterController::class, 'getAll']);
-        Route::middleware('validate.id')->get('/{id}', [CharacterController::class, 'getById']);
-        Route::middleware('validate.id')->get('/{id}/actor', [CharacterController::class, 'actor']);
-        Route::post('', [CharacterController::class, 'create']);
-        Route::middleware('validate.id')->delete('/{id}', [CharacterController::class, 'delete']);
-        Route::middleware('validate.id')->patch('/{id}', [CharacterController::class, 'modify']);
-    });
-
-    Route::prefix('/microphones')->group(function () {
-        Route::get('', [MicrophoneController::class, 'getAll']);
-        Route::middleware('validate.id')->get('/{id}', [MicrophoneController::class, 'getById']);
-        Route::middleware('validate.id')->get('/{id}/actor', [MicrophoneController::class, 'actor']);
-        Route::post('', [MicrophoneController::class, 'create']);
-        Route::middleware('validate.id')->delete('/{id}', [MicrophoneController::class, 'delete']);
-        Route::middleware('validate.id')->patch('/{id}', [MicrophoneController::class, 'modify']);
-    });
+Route::prefix('/actors')->group(function () {
+    Route::get('', [ActorController::class, 'getAll']);
+    Route::middleware('validate.id')->get('/{id}', [ActorController::class, 'getById']);
+    Route::middleware('validate.id')->get('/{id}/characters', [ActorController::class, 'characters']);
+    Route::middleware('validate.id')->get('/{id}/microphone', [ActorController::class, 'microphone']);
+    Route::post('', [ActorController::class, 'create']);
+    Route::middleware('validate.id')->delete('/{id}', [ActorController::class, 'delete']);
+    Route::middleware('validate.id')->patch('/{id}', [ActorController::class, 'modify']);
 });
 
+Route::prefix('/characters')->group(function () {
+    Route::get('', [CharacterController::class, 'getAll']);
+    Route::middleware('validate.id')->get('/{id}', [CharacterController::class, 'getById']);
+    Route::middleware('validate.id')->get('/{id}/actor', [CharacterController::class, 'actor']);
+    Route::post('', [CharacterController::class, 'create']);
+    Route::middleware('validate.id')->delete('/{id}', [CharacterController::class, 'delete']);
+    Route::middleware('validate.id')->patch('/{id}', [CharacterController::class, 'modify']);
+});
